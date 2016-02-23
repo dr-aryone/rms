4a8b3f231abdb95ddce74ee0c241cee52606db75
Sara <admin@truthyfalsey.com>
2016-02-23 10:53:48 -0500

    Changing database path

--- config/env/production.js
+++ config/env/production.js
@@ -1,4 +1,4 @@
 module.exports = {
-  db: 'mongodb://localhost/newrms',
+  db: 'mongodb://heroku_3pnv7czn:heroku_3pnv7czn@ds031947.mongolab.com:31947/heroku_3pnv7czn',
   sessionSecret: 'pineapple'
 };
