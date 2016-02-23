2a9381b72ae00fed165ee8ab8b71e51ec2b83cdd
Sara <admin@truthyfalsey.com>
2016-02-23 11:46:47 -0500

    Troubleshooting connection to db

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,8 @@ var config = require('./config'),
     mongoose = require('mongoose');
 
 module.exports = function () {
-  var db = mongoose.connect(config.db);
+  //var db = mongoose.connect(config.db);
+  var db = mongoose.connect(db: 'mongodb://heroku_3pnv7czn:pijtp16q3acfab1a8tvnphc8cl@ds031947.mongolab.com:31947/heroku_3pnv7czn');
 
   require('../app/models/user');
   require('../app/models/concert');
